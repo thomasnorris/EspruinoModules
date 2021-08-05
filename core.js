417b530539fa5c4a93e9abe460120a5b9cf7cb27
Thomas Norris <tom.norris135@gmail.com>
2021-08-05 09:28:42 -0400

    add core

---
+++ core.js
@@ -0,0 +1,64 @@
+var _core = function (settings, afterInit) {
+    var self = this;
+    var modules = {
+        storage: require('Storage')
+    };
+
+    this.settings = settings;
+
+    this.fn = {
+        init: function () {
+            // no init
+
+            self.fn.logInfo('Core initialized.');
+
+            if (typeof afterInit == 'function') {
+                afterInit();
+            }
+        },
+        readStorage: function (key) {
+            console.log('Reading ' + key + ' from Storage...');
+
+            var value = modules.storage.read(key);
+            if (value == undefined) {
+                console.log(key + ' in Storage is undefined!');
+            }
+
+            return value;
+        },
+        msToS: function (ms) {
+            return ms / 1000;
+        },
+        nullCoalesce: function (lhs, rhs) {
+            // equivalent to lhs ?? rhs
+            if (lhs == false || lhs == undefined) {
+                return rhs;
+            }
+
+            return lhs;
+        },
+        logInfo: function (msg) {
+            msg = 'INFO: ' + msg;
+            console.log(msg);
+            return msg;
+        },
+        logWarn: function (msg) {
+            msg = 'WARNING: ' + msg;
+            console.log(msg);
+            return msg;
+        },
+        logError: function (msg) {
+            msg = 'ERROR: ' + msg;
+            console.log(msg);
+            return msg;
+        }
+    };
+
+    // set this.settings again using nullCoalesce function from above
+    this.settings = self.fn.nullCoalesce(settings, {});
+
+    // init
+    self.fn.init();
+};
+
+exports.core = _core;
